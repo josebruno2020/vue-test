257b2f2b988671d6992e06561941c246a09ffd9e
José Bruno <60969043+josebruno2020@users.noreply.github.com>
2021-06-29 12:01:19 -0300

    Primeiro teste, montando o component Lance

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <div id="app">
-    <div
-      class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow"
-    >
-      <h5 class="my-0 mr-md-auto font-weight-normal">Leiloeiro</h5>
-      <router-link class="btn btn-outline-primary" to="/leilao">Novo leilão</router-link>
+    <div id="app">
+        <div
+        class="d-flex  justify-content-between align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow"
+        >
+            <span class="d-flex align-items-center">
+                <router-link class="text-dark" to="/">
+                    <h5 class="my-0 mr-md-auto font-weight-normal">Leiloeiro</h5>
+                </router-link>
+            </span>
+            <span>
+                <router-link class="btn btn-outline-primary" to="/leilao">Novo leilão</router-link>
+            </span>
+        </div>
+        <router-view />
     </div>
-    <router-view />
-  </div>
 </template>
