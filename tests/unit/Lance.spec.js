72402097d6a3dca3b9920db5516529be5966bde5
José Bruno <60969043+josebruno2020@users.noreply.github.com>
2021-06-30 19:20:46 -0300

    implementando mais dois testes no componente Lance

--- tests/unit/Lance.spec.js
+++ tests/unit/Lance.spec.js
@@ -3,5 +3,28 @@ import { mount } from '@vue/test-utils'
 
 test('não aceita valor negativo', () => {
   const wrapper = mount(Lance)
-  expect(wrapper).toBeTruthy()
+  const input = wrapper.find('input')
+  input.setValue(-199)
+  wrapper.trigger('submit')
+  const lancesEmitidos = wrapper.emitted('novo-lance')
+  expect(lancesEmitidos).toBeUndefined()
+})
+
+test('emite o evento quando o valor é positivo', () => {
+  const wrapper = mount(Lance)
+  const input = wrapper.find('input')
+  input.setValue(100)
+  wrapper.trigger('submit')
+  const lancesEmitidos = wrapper.emitted('novo-lance')
+  expect(lancesEmitidos).toHaveLength(1)
+})
+
+test('emite o valor esperado de um lance valido', () => {
+  const wrapper = mount(Lance)
+  const input = wrapper.find('input')
+  input.setValue(100)
+  wrapper.trigger('submit')
+  const lancesEmitidos = wrapper.emitted('novo-lance')
+  const lance = parseInt(lancesEmitidos[0][0])
+  expect(lance).toBe(100)
 })
